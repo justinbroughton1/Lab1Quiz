ce4ccd080e59db9633af95830bc57b012ac28afe
justinbroughton1 <justin.broughton@oit.edu>
2016-01-12 19:53:53 +0000

    Quiz1

---
+++ tokens.h
@@ -0,0 +1,19 @@
+#pragma once
+
+//**************************************
+// tokens.h
+//
+// Defines the values for the tokens returned by yylex
+//
+// Author: Phil Howard 
+// phil.howard@oit.edu
+//
+// Date: Dec 30, 2015
+//
+
+#define STUFF 1000
+#define OTHER 1001
+#define BLOB1 1002
+#define BLOB2 1003
+#define JUNK_TOKEN 2000
+
